27164ad38d5439e0c52b48d5e2003523879249bc
shsa <shefersa@yandexlyceum.ru>
2024-11-30 16:21:43 +0700

    Решили add new function

--- program.py
+++ program.py
@@ -1,6 +1,10 @@
 def main():
     print('My first git program')
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to
+
 
 if __name__ == '__main__':
     main()
